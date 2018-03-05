1aaad925ba2af7e17206d4949cdccd613cb3e78c
Bas de Ruijter <Bas@Funky-System.org>
2018-03-05 20:59:57 +0100

    added quotes for parameters in build script

--- runtime/build.sh
+++ runtime/build.sh
@@ -4,8 +4,8 @@ set -e
 ARCH=$3
 if [ -z ${3+x} ]; then ARCH=32; fi
 
-for file in $1/*.wckd
+for file in "$1/*.wckd"
 do
     echo Compiling runtime object $(basename -s .wckd "$file")
-    wickedc --arch $ARCH -K "$file" -o $2/$(basename -s .wckd "$file").funk
+    wickedc --arch $ARCH -K "$file" -o "$2/$(basename -s .wckd "$file").funk"
 done
