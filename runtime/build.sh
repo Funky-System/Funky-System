3d081bd560adcc2dc3d51642f8491caf936d6fc3
Bas de Ruijter <Bas@Funky-System.org>
2018-03-05 21:06:52 +0100

    fixed wrong quotation

--- runtime/build.sh
+++ runtime/build.sh
@@ -4,7 +4,7 @@ set -e
 ARCH=$3
 if [ -z ${3+x} ]; then ARCH=32; fi
 
-for file in "$1/*.wckd"
+for file in "$1"/*.wckd
 do
     echo Compiling runtime object $(basename -s .wckd "$file")
     wickedc --arch $ARCH -K "$file" -o "$2/$(basename -s .wckd "$file").funk"
